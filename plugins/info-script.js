69a93e7f0fe4305232ff9dea56cd857ff655b78e
Bellfroutcs <87676463+Bellfroutcs@users.noreply.github.com>
2022-11-14 17:46:29 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtube.com/channel/UCTVeRHM0v0Hqv_-O8yVkFpw*`
+let str = `*https://youtube.com/channel/UCL_UuXsDGFLdwARCwytYFBA*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/inizenscuy",
+    mediaUrl: "https://Instagram.com/ananda.alif.rizky",
     title: 'Albedo-MultiDevice',
     body: 'want source code?',
     thumbnail: thumb,
